7dd26510fe4949afcdede26847c25d1983a4af97
answere3030 <answere3030@gmail.com>
2022-04-24 19:30:05 +0900

    2022.04.24 1.Layout setting complete

---
+++ app/src/main/java/com/answere2022/musicplayer/Fragment_player.kt
@@ -0,0 +1,11 @@
+package com.answere2022.musicplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Fragment_player : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_player)
+    }
+}
